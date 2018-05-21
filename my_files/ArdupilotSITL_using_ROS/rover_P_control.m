956eaaa32729b9893cbf98fa5398dafbd84ab2a2
shaileshprusty <shailesh.prusty@gmail.com>
2018-05-21 17:33:44 +0530

    Indentation fixed

--- my_files/ArdupilotSITL_using_ROS/rover_P_control.m
+++ my_files/ArdupilotSITL_using_ROS/rover_P_control.m
@@ -9,30 +9,33 @@ Vk = 500;
 kp = 0.5;
 
 while(1)
-position_LA = mysub1.LatestMessage.Latitude;
-position_LO = mysub1.LatestMessage.Longitude;
+    position_LA = mysub1.LatestMessage.Latitude;
+    position_LO = mysub1.LatestMessage.Longitude;
 
-theta = mysub2.LatestMessage.Data;
+    theta = mysub2.LatestMessage.Data;
 
-LOS_dist = sqrt((target_LO-position_LO)*(target_LO-position_LO)+(target_LA-position_LA)*(target_LA-position_LA));
+    LOS_dist = sqrt((target_LO-position_LO)*(target_LO-position_LO)+(target_LA-position_LA)*(target_LA-position_LA));
 
-alpha = atan2((target_LO-position_LO),(target_LA-position_LA));
+    alpha = atan2((target_LO-position_LO),(target_LA-position_LA));
 
-beta = 3.14*theta/180.0 - alpha;
-  if (beta >= 3.14)
-    beta = beta - 2*3.14;
-  end
-  if (beta <= -3.14)
-    beta = beta + 2*3.14;
-  end
-  if (LOS_dist >= 0.00012)   
-    pubmsg.Linear.X = Vk;
-    pubmsg.Angular.Z = kp*beta;
-  else
-    pubmsg.Linear.X = 0;
-    pubmsg.Linear.Y = 0;
-    pubmsg.Angular.Z = 0;
-  end
+    beta = 3.14*theta/180.0 - alpha;
+    
+    if (beta >= 3.14)
+        beta = beta - 2*3.14;
+    end
+    
+    if (beta <= -3.14)
+        beta = beta + 2*3.14;
+    end
+    
+    if (LOS_dist >= 0.00012)
+        pubmsg.Linear.X = Vk;
+        pubmsg.Angular.Z = kp*beta;
+    else
+        pubmsg.Linear.X = 0;
+        pubmsg.Linear.Y = 0;
+        pubmsg.Angular.Z = 0;
+    end
   
-send(mypub , pubmsg);
+    send(mypub , pubmsg);
 end
--- my_files/ArdupilotSITL_using_ROS/turtlesim_P_control.m
+++ my_files/ArdupilotSITL_using_ROS/turtlesim_P_control.m
@@ -6,32 +6,30 @@ Y = 10;
 k = 1.5;
 
 while(1)
-recvMsg = mysub.LatestMessage;
-x = recvMsg.X;
-y = recvMsg.Y;
-theta = recvMsg.Theta;
-d=sqrt((X-x)*(X-x) + (Y-y)*(Y-y));
+    recvMsg = mysub.LatestMessage;
+    x = recvMsg.X;
+    y = recvMsg.Y;
+    theta = recvMsg.Theta;
+    d=sqrt((X-x)*(X-x) + (Y-y)*(Y-y));
     phi = atan2((Y-y),(X-x));
     beta = phi - theta;
+    
     if (beta >= 3.14)
-          beta = beta - 2*3.14;
-          
+        beta = beta - 2*3.14;
     end
+    
     if (beta <= -3.14)
-          beta = 2*3.14 + beta;
+        beta = 2*3.14 + beta;
     end 
         
     if(d <= 0.2)
-	
 		pubmsg.Linear.X=0;
 		pubmsg.Angular.Z=0;    
-
-	else
-    	
+    else
 		pubmsg.Linear.X=1;
 		pubmsg.Angular.Z=k*beta; 
     end
 
-send(mypub , pubmsg);
+    send(mypub , pubmsg);
 
 end

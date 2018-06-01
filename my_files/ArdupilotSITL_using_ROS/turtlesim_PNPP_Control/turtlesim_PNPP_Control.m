8d1cab2808daedb8a9c23014ba5b541aecf50e35
shaileshprusty <shailesh.prusty@gmail.com>
2018-06-01 14:13:16 +0530

    Bugs fixed - still error prone

--- my_files/ArdupilotSITL_using_ROS/turtlesim_PNPP_Control/turtlesim_PNPP_Control.m
+++ my_files/ArdupilotSITL_using_ROS/turtlesim_PNPP_Control/turtlesim_PNPP_Control.m
@@ -27,11 +27,11 @@ sys = turtlesim_PNPP_RealVehicleROS(target, ...
                              
 sys.initialCondition = {[attacker_position.X;
                          attacker_position.Y;
-                         sqrt((obj.target(1) - attacker_position.X)^2 + (obj.target(2) - attacker_position.Y)^2);
-                         atan2(obj.target(2) - attacker_position.Y, obj.target(1) - attacker_position.X);
+                         sqrt((target(1) - attacker_position.X)^2 + (target(2) - attacker_position.Y)^2);
+                         atan2(target(2) - attacker_position.Y, target(1) - attacker_position.X);
                          attacker_position.Theta]};
                      
-sys.controller = turtlesim_IController(@(t,x) ((((-1).^floor(ceil(t/obj.switchGuidanceLaw)))+1)/2)*(obj.N*(-obj.vm*sin(x(5)-x(4)))/x(3)))+(1-((((-1).^floor(ceil(t/obj.switchGuidanceLaw)))+1)/2))*(((-obj.K*(x(5)-x(4)))/obj.vm));
+sys.controller = turtlesim_PNPP_IController(@(t,x) ((((-1).^floor(ceil(t/switchGuidanceLaw)))+1)/2)*(N*(-vm*sin(x(5)-x(4)))/x(3)))+(1-((((-1).^floor(ceil(t/switchGuidanceLaw)))+1)/2))*(((K*(x(5)-x(4)))/vm));
 
 va = VirtualArena(sys,...
     'StoppingCriteria'  , @(t,x,sysList)sqrt((sys.x(1)-sys.x(3))^2 + (sys.x(2)-sys.x(2))^2)<=0.1,...
